727b9d2a4d689e2d7df7b361f19a35a77628c671
turangi <stuartfe@hotmail.com>
2017-05-31 16:20:09 -0700

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,61 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  // Challenge for Lecture 74:
+  // Make a test case that verifies that we do *not* create a
+  // todo when we send bad data.
+  // this will be similar to the above case, but, with no data being
+  // posted (an empty object is passed), a 400 status should be returned, there is no
+  // need to verify the result text, and the length should be 0.
+  it('should not create todo with invalid body data', (done) => {
+
+      request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+
+
+});

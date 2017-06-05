09f6175ebf02ebe16aafce60d55b39bb24362855
turangi <stuartfe@hotmail.com>
2017-06-05 12:10:54 -0700

    Add test cases in server.test.js for GET /todos/:id.  Minor syntax cleanup in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-// local imports
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// library imports kept a line-space from local imports
 const {ObjectID} = require('mongodb');
 
-let {mongoose} = require('./db/mongoose');
+const {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
@@ -13,16 +11,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// Inside a REST api,there are the basic CRUD operations
-// To create a resourse, use the POST http method to send that resource
-// as a body.  This means that when we want make a new Todo, we'll send a
-// JSON object over the server.  This object
-// will have a text property. The server will GET the text property, create
-// the new model, and send the complete model with the ID, the completed
-// property and the completedAt back to the client.
-
-// set up an express route, using the two arguments URL and callback with
-// request and response objects
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
@@ -43,40 +31,11 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// Challenge: Lecture 78:  Create an API route for fetching an individual todo
-//
-// How to fetch a variable that is passed in from a URL:
-// GET /todos/1234324 - this part of the URL needs to be dynamic
-// use a URL parameter, with colon followed by a name
-/* *** How I did it ***
-
-app.get('/todos/:id', (req, res) => {
-  let id = req.params.id;
-
-  // // Check for valid id using isValid
-  // if(!ObjectID.isValid(id)) {
-  //   res.status(404).send('Id not valid.');
-  // };
-
-
-  // Next, query the database by using findById
-  Todo.findById(id).then((todo) => {
-    if(!todo) {
-      res.status(404).send('Todo was not found');
-    }
-    res.send(todo);
-  }).catch((e) => res.status(400).send('Not a valid URL'));
-});
-
-**** End of how I did it ***/
-
-
-/*** How the instructor did it ***/
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
 
   if(!ObjectID.isValid(id)) {
-    return res.status(404).send();  //note: I needed to add a return prefix
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
@@ -84,12 +43,11 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todo}); // Note: This is equivalent to res.send({todo: todo}), gratis ES6
+    res.send({todo});
   }).catch((e) => {
-    res.status(400).send();  // note:  I didn't use braces here... :^(
+    res.status(400).send();
   });
 });
-
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -56,6 +59,7 @@ describe('POST /todos', () => {
           done();
         }).catch((e) => done(e));
       });
+    });
   });
 
   describe('GET /todos', () => {
@@ -69,4 +73,58 @@ describe('POST /todos', () => {
       .end(done);
     });
   });
-});
+
+  // When we pass in a valid ID that does match a doc, the doc comes back (#3 below)
+  describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+      request(app)  // initiate supertest request
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+// The above case was #3 from the list below.  Now, create both 1 and 2.
+// Instructor provides starting point(s) (note:  We are still inside the
+// describe() block):
+  it('should return a 404 if todo not found', (done) => {
+    // make a request using a real object id, using the toHexString method
+    // that means it will be a valid id, but won't be found in the collection,
+    // thus returning a 404.
+    // the id-with-no-todo doesn't exist yet, so let's make one:
+    // first, create that object id as follows:
+    let hexId = new ObjectID().toHexString();
+
+    // make the request...
+    request(app)
+      .get(`/todos/${hexId}`) // get request, passing in template string with hexId
+      .expect(404)
+      .end(done);
+  });
+
+
+  // check for invalid id, which will return a 404 error
+  it('should return 404 for non-object ids', (done) => {
+    // /todos/123 --> this is a valid URL, but not a valid objectID
+    request(app)
+      .get('/todos/123abc') // URL with invalid objectID
+      .expect(404)
+      .end(done);
+  })
+}); // end describe block
+
+
+
+// Lecture 79 Notes:
+/*  In this lecture, we'll be creating three test cases for the route that
+    fetches an individual Todo item.
+    The three tests verify that:
+  1)  When we pass in an invalid ObjectID, we get a 404 response.
+
+  2)  When we pass in a valid ObjectID, but it doesn't match a doc, we get a 404
+      response.
+
+  3)  When we do pass in a valid ObjectID that does match a doc, that the
+      document actually comes back in the response body.
+*/

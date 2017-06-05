2ce5c62f89d10b99a3c36c399bc60d5bd50bb495
turangi <stuartfe@hotmail.com>
2017-06-05 13:31:43 -0700

    Fixed some egregious typos in server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
-const app = express
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -50,7 +49,7 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 

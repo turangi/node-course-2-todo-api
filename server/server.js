7e7717fdce537f220b4c55fbae0b8c22db1d7459
turangi <stuartfe@hotmail.com>
2017-06-07 07:58:46 -0700

    Add PATCH /todos:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -51,6 +52,7 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) =>{
   let id = req.params.id;
+
   if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
@@ -64,6 +66,37 @@ app.delete('/todos/:id', (req, res) =>{
     res.status(400).send();
   });
 });
+
+// This route is going to use an HTTP Patch method.
+// Patch is what is used to update a resource
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  // make query to update the database, similar to the method used in the
+  // playground file "mongodb-update".
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+}); // end patch route
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
